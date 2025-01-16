958a55b507b9d6a729fb9ac846580edb5710e3d4
Patryk <patrykmigala0@gmail.com>
2025-01-16 17:01:40 +0100

    navigation for phones

--- source/javascript/defaultScript.js
+++ source/javascript/defaultScript.js
@@ -1,32 +1,42 @@
-let arrow = document.querySelector('.closeNav');
+let arrow = document.querySelector('.closeArrow');
+//let bg_nav = document.querySelector('.bg-nav');
 
 arrow.addEventListener('click', () => {
-    let nav_open = document.querySelector('nav');
+    let nav_open = document.querySelector('.nav-pc');
 
     nav_open.classList.toggle('nav-open');
-    arrow.classList.toggle('rotate-arrow')
+    arrow.classList.toggle('rotate-arrow');
+    //bg_nav.classList.toggle('bg-nav-color');
 
 });
 
 
-if(window.innerWidth <= 1000) {
+if (window.innerWidth <= 650) {
     const header = document.querySelector('header');
+    const nav_mobile = document.querySelector('.nav-mobile');
 
     let prevScrollpos = window.scrollY;
-    
+
     window.addEventListener('scroll', () => {
         let currScrollpos = window.scrollY;
-    
-        if(prevScrollpos < currScrollpos) {
+
+        if (prevScrollpos < currScrollpos) {
             header.classList.remove('header-show');
             header.classList.add('header-hide');
+
+            nav_mobile.classList.remove('nav-show');
+            nav_mobile.classList.add('nav-hide');
+
         }
         else {
             header.classList.remove('header-hide');
             header.classList.add('header-show');
+
+            nav_mobile.classList.remove('nav-hide');
+            nav_mobile.classList.add('nav-show');
         }
 
         prevScrollpos = currScrollpos;
     })
-    
+
 }
