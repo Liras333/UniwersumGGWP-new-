0311a7b2211c29e4cd1ea65923e92d94fc1b1425
Patryk <patrykmigala0@gmail.com>
2025-02-25 18:05:26 +0100

    changes gb to rpg

--- source/javascript/postacDnia.js
+++ source/javascript/postacDnia.js
@@ -1,62 +1,44 @@
-const imgURL = [
-    "./source/img/Uniwersum/Postacie/Adonis4.jpg",
-    "./source/img/Uniwersum/Postacie/Brom.jpg",
-    "./source/img/Uniwersum/Postacie/Bulgerbass.jpg",
-    "./source/img/Uniwersum/Postacie/dariel bermid.jpg",
-    "./source/img/Uniwersum/Postacie/Ebendor.jpg",
-    "./source/img/Uniwersum/Postacie/er' Garoth.jpg",
-    "./source/img/Uniwersum/Postacie/hildebrand.jpg",
-    "./source/img/Uniwersum/Postacie/Kirithor.jpg",
-    "./source/img/Uniwersum/Postacie/Logan_Szkaratny_Mag.jpg",
-    "./source/img/Uniwersum/Postacie/marstofeliusz.jpg",
-    "./source/img/Uniwersum/Postacie/Nasir_Yadav2.jpg",
-    "./source/img/Uniwersum/Postacie/Swen.jpg",
-    "./source/img/Uniwersum/Postacie/Zigorok-Enraged2.jpg"
-]
-
-const names = [
-    "Adonis4",
-    "Brom",
-    "Bulgerbass",
-    "darien bermid",
-    "Ebendor",
-    "er' Garoth",
-    "hildebrand",
-    "Kirithor",
-    "Logan - Szkarłatny Mag",
-    "Malkolm",
-    "Marstofeliusz",
-    "Swen",
-    "Zigorok"
-]
+const postacie = [
+    { imgUrl: "./source/img/Uniwersum/Postacie/Adonis4.jpg", name: "Adonisx`" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Brom.jpg", name: "Brom" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Bulgerbass.jpg", name: "Bulgerbass" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/darien bermid.jpg", name: "darien bermid" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Ebendor.jpg", name: "Ebendor" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/er' Garoth.jpg", name: "er' Garoth" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/hildebrand.jpg", name: "hildebrand" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Kirithor.jpg", name: "Kirithor" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Logan_Szkaratny_Mag.jpg", name: "Logan - Szkarłatny Mag" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/marstofeliusz.jpg", name: "Marstofeliusz" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Malkolm.jpg", name: "Malkolm" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Swen.jpg", name: "Swen" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Zigorok-Enraged2.jpg", name: "Zigorok" },
+];
 
 let postac = document.querySelector('.postac img');
 let nazwa = document.querySelector('.nazwa');
 
 
 function postacDnia(year, month, day) {
-    let dataToday = new Date(year, month - 1, day );
+    let dataToday = new Date(year, month - 1, day);
     let dataYastarday = new Date(year, month - 1, day - 1);
 
     let minutesToday = Math.floor(dataToday.getTime());
     let minutesYastarday = Math.floor(dataYastarday.getTime());
 
-    let index = minutesToday % names.length ;
-    let index2 = minutesYastarday % names.length ;
+    let index = minutesToday % postacie.length;
+    let index2 = minutesYastarday % postacie.length;
 
     let newIndex = index;
-    for(let i = 0; index == index2; i++){
+    for (let i = 0; index == index2; i++) {
         newIndex++;
-        postac.src = imgURL[newindex];
-        nazwa.textContent = names[newindex];
+        postac.src = postacie[newindex].imgUrl;
+        nazwa.textContent = postacie[newindex].name;
     }
 
-    postac.src = imgURL[index];
-    nazwa.textContent = names[index];
+    postac.src = postacie[index].imgUrl;
+    nazwa.textContent = postacie[index].name;
 }
 
 let data = new Date();
 
-postacDnia(data.getFullYear(), data.getMonth(), data.getDate())
-
-
+postacDnia(data.getFullYear(), data.getMonth(), data.getDate());
