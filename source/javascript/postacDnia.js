ba5b49a10dac9b4e0c9b900ae6d2ecded7b50063
Patryk <patrykmigala0@gmail.com>
2025-02-27 19:16:18 +0100

    changes in postac dnia

--- source/javascript/postacDnia.js
+++ source/javascript/postacDnia.js
@@ -1,17 +1,22 @@
 const postacie = [
-    { imgUrl: "./source/img/Uniwersum/Postacie/Adonis4.jpg", name: "Adonisx`" },
-    { imgUrl: "./source/img/Uniwersum/Postacie/Brom.jpg", name: "Brom" },
-    { imgUrl: "./source/img/Uniwersum/Postacie/Bulgerbass.jpg", name: "Bulgerbass" },
-    { imgUrl: "./source/img/Uniwersum/Postacie/darien bermid.jpg", name: "darien bermid" },
-    { imgUrl: "./source/img/Uniwersum/Postacie/Ebendor.jpg", name: "Ebendor" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Adonis4.jpg", name: "Adonis - Prawdziwa Elita" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Brom.jpg", name: "Brom - Opętany" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Bulgerbass.jpg", name: "Bülgerbass - Postrach Shekki" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Cosmos.png", name: "Cosmos - Astralny Smok" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/darien bermid.jpg", name: "Darien Bermid - Smokobójca" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Ebendor.jpg", name: "Ebendor - Wulkaniczny Tytan" },
     { imgUrl: "./source/img/Uniwersum/Postacie/er' Garoth.jpg", name: "er' Garoth" },
-    { imgUrl: "./source/img/Uniwersum/Postacie/hildebrand.jpg", name: "hildebrand" },
-    { imgUrl: "./source/img/Uniwersum/Postacie/Kirithor.jpg", name: "Kirithor" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/hildebrand.jpg", name: "Hildebrand - Upadły Mag" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Kirithor.jpg", name: "Kirithor Udor - Astralny Poszukiwacz" },
     { imgUrl: "./source/img/Uniwersum/Postacie/Logan_Szkaratny_Mag.jpg", name: "Logan - Szkarłatny Mag" },
-    { imgUrl: "./source/img/Uniwersum/Postacie/marstofeliusz.jpg", name: "Marstofeliusz" },
-    { imgUrl: "./source/img/Uniwersum/Postacie/Malkolm.jpg", name: "Malkolm" },
-    { imgUrl: "./source/img/Uniwersum/Postacie/Swen.jpg", name: "Swen" },
-    { imgUrl: "./source/img/Uniwersum/Postacie/Zigorok-Enraged2.jpg", name: "Zigorok" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/marstofeliusz.jpg", name: "Marstofeliusz - Władca Nak Sasoom" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Malkolm.jpg", name: "Malkolm - Kreator" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/rodrigo.png", name: "Rodrigo - Hochsztapler" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Swen.jpg", name: "Swen - Męczennik" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Xolgothan.png", name: "Xolgothan - Kosmiczny Niszczyciel" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Zigorok-Enraged2.jpg", name: "Zigorok - Pradawny Demon" },
+    { imgUrl: "./source/img/Uniwersum/Postacie/Logan_Szkaratny_Mag.jpg", name: "Logan - Szkarłatny Mag" }
+    
 ];
 
 let postac = document.querySelector('.postac img');
@@ -19,7 +24,7 @@ let nazwa = document.querySelector('.nazwa');
 
 
 function postacDnia(year, month, day) {
-    let dataToday = new Date(year, month - 1, day );
+    let dataToday = new Date(year, month - 1, day);
     let dataYastarday = new Date(year, month - 1, day - 1);
 
     let minutesToday = Math.floor(dataToday.getTime());
@@ -28,11 +33,12 @@ function postacDnia(year, month, day) {
     let index = minutesToday % postacie.length;
     let index2 = minutesYastarday % postacie.length;
 
+
     let newIndex = index;
     for (let i = 0; index == index2; i++) {
         newIndex++;
-        postac.src = postacie[newindex].imgUrl;
-        nazwa.textContent = postacie[newindex].name;
+        postac.src = postacie[newIndex].imgUrl;
+        nazwa.textContent = postacie[newIndex].name;
     }
 
     postac.src = postacie[index].imgUrl;
